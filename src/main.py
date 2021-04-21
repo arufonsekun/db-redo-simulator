9ed7641d21e462e0ba01a8b6d288d7ae989234db
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2021-04-21 15:59:55 -0300

    Create table given a log file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import argparse
-from .parser import *
-from .db import *
+from . import parser
+from . import db
+
+TABLE_NAME = "redo_simulator"
 
 
 def get_cmd_args():
@@ -21,10 +23,40 @@ def get_cmd_args():
     return arguments
 
 
+def create_table(conn, columns_values):
+    columns = tuple(columns_values.keys())
+    values = tuple(columns_values.values())
+
+    db.create(conn, TABLE_NAME, columns)
+    db.insert(conn, TABLE_NAME, columns, values)
+
+
+def drop_table(conn):
+    """Drop database table
+
+    Args:
+        conn (connection): Postgres object connection
+    """
+    db.drop(TABLE_NAME)
+
+
 def main():
     args = get_cmd_args()
-    print(args.log)
+    log_file = args.log
+    conn = db.connect()
+    print(type(conn))
 
+    try:
+        with open(log_file, "r") as log:
+            columns_values = parser.get_columns_names_and_values(log)
+            
+            create_table(conn, columns_values)
+            db.close(conn)
+
+    except Exception:
+        conn.close()
+
+    
 if __name__ == "__main__":
     main()
 
